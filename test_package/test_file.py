342429f04d93cdef3e5eee24ef45a0f94de95311
Alex Paladin <alex_paladin@mail.ru>
2020-05-11 18:43:41 +0300

    Dev changes

--- test_package/test_file.py
+++ test_package/test_file.py
@@ -1,3 +1,5 @@
+# test row dev
+
 def reverse_name(name1):
     list1 = list(name1)
     list1.reverse()
@@ -7,8 +9,6 @@ def diff_letters(name1):
     set1 = set(name1)
     return len(set1)
 
-
-
 print('Enter your name:')
 a = input()
 print("Enter your friend's name:")
@@ -16,6 +16,3 @@ b = input()
 
 print('Your reversed name is:',reverse_name(a))
 print('Your name contains',diff_letters(a),' different letters')
-
-
-
