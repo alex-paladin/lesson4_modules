6331149a94a7dcba5261d04b4019460b7430a406
Alex Paladin <alex_paladin@mail.ru>
2020-05-11 19:38:56 +0300

    d

---
+++ test_package/test_file2.py
@@ -0,0 +1,16 @@
+def reverse_name(name1):
+    list1 = list(name1)
+    list1.reverse()
+    print(" ".join(list1))
+
+def diff_letters(name1):
+    set1 = set(name1)
+    return len(set1)
+
+print('Enter your name:')
+a = input()
+print('Enter your friends name:')
+b = input()
+
+print('Your reversed name is:',reverse_name(a))
+print('Your name contains',diff_letters(a),' different letters')
